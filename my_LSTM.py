62db47e47fbf719b6aa3100885b045f6b1f99542
Baruch <bakshtb@gmail.com>
2022-11-13 12:21:05 +0200

    pose

--- my_LSTM.py
+++ my_LSTM.py
@@ -10,6 +10,8 @@ from tensorflow.keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 from poseEstimation import image_to_pose
+from tqdm import tqdm
+
 
 FALL = 1
 NOT_FALL = 0
@@ -19,9 +21,18 @@ def get_data_from_images(folder_path):
     x = []
     y = []
     for folder in os.listdir(folder_path):
-        for file in os.listdir(folder_path + '/' + folder)[:3]:
-            x.append(image_to_pose(folder_path + '/' + folder + '/' + file)[0])
-            y.append(FALL if folder == 'fall' else NOT_FALL)
+        for file in tqdm(os.listdir(folder_path + '/' + folder)):
+            pose = image_to_pose(folder_path + '/' + folder + '/' + file)
+            if len(pose) >= 1:
+                pose = pose[0][7:]
+                _pose = []
+                for i in range(17):  # 17 keypoints
+                    x = pose[3 * i]
+                    y = pose[3 * i + 1]
+                    _pose.append(x)
+                    _pose.append(y)
+                x.append(_pose)
+                y.append(FALL if folder == 'fall' else NOT_FALL)
     return pd.DataFrame({'x': x, 'y': y})
 
 
@@ -49,4 +60,19 @@ def train(from_images=True):
     # dataset = scaler.fit_transform(dataset)
 
 
-train()
+def show_pose(image_path):
+    pose = image_to_pose(image_path)
+
+    if len(pose) >= 1:
+        pose = pose[0][7:]
+        plt.figimage(plt.imread(image_path))
+        for i in range(17):
+            x = pose[3 * i]
+            y = pose[3 * i + 1]
+            print(x, y)
+            plt.scatter(x, y)
+        plt.savefig('pose.png')
+
+
+# train()
+show_pose('C:\\Users\\bakshtb\\Downloads\\test.webp')
